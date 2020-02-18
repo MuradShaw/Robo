e2b27df519c344210d6249bc32c496c4df6b8ef9
MuradShaw <60895889+MuradShaw@users.noreply.github.com>
2020-02-18 13:41:12 -0500

    Add files via upload

--- voice_assistant/Form1.Designer.cs
+++ voice_assistant/Form1.Designer.cs
@@ -41,7 +41,7 @@
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(131, 25);
 			this.label1.TabIndex = 1;
-			this.label1.Text = "Ultra Instinct";
+			this.label1.Text = "ROBO (Base)";
 			this.label1.Click += new System.EventHandler(this.label1_Click);
 			// 
 			// inputField
